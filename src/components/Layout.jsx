089028becb91e071dbc52b552e7f70af5ff8f295
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-20 12:00:09 -0600

    diseño Layout principal

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,11 +3,15 @@ import { Outlet } from "react-router-dom";
 // Layout principal
 export const Layout = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold">
-        Administrador de Clientes - CRM React
-      </h1>
-      <Outlet />
+    <div className="md:flex md:min-h-screen">
+      <div className="md:w-1/4 bg-blue-900 p-10">
+        <h1 className="font-bold text-center text-2xl text-white">
+          CRM - Clientes
+        </h1>
+      </div>
+      <div className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+        <Outlet />
+      </div>
     </div>
   );
 };
