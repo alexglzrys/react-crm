5ae6a6eacb4bf9473b2153524d044ed1124c4790
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-20 12:23:15 -0600

    navegar entre rutas

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,43 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 
 // Layout principal
 export const Layout = () => {
+  // información de la ruta actual
+  const location = useLocation();
+
+  /**
+   * El componente NavLink ya viene preparado para saber si el enlace es la ruta actual y responder con el CSS necesario
+   * y evitar usar el hook useLocation
+   * pero, al parecer sigue presentando problemas en rutas raiz.
+   */
+
   return (
     <div className="md:flex md:min-h-screen">
-      <div className="md:w-1/4 bg-blue-900 p-10">
+      <aside className="md:w-1/4 bg-blue-900 p-10">
         <h1 className="font-bold text-center text-2xl text-white">
           CRM - Clientes
         </h1>
-      </div>
+        <nav className="mt-10">
+          <Link
+            to="/"
+            className={`${
+              location.pathname === "/" ? "text-blue-300" : "text-white"
+            } block hover: mt-1`}
+          >
+            Clientes
+          </Link>
+          <Link
+            to="clientes/nuevo"
+            className={`${
+              location.pathname === "/clientes/nuevo"
+                ? "text-blue-300"
+                : "text-white"
+            } block hover: mt-1`}
+          >
+            Nuevo cliente
+          </Link>
+        </nav>
+      </aside>
       <div className="md:w-3/4 p-10 md:h-screen overflow-scroll">
         <Outlet />
       </div>
