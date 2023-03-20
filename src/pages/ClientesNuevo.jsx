e61747213720b7a8c1bd5cfc77ad60de69576a96
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-20 13:33:45 -0600

    página registrar cliente

--- src/pages/ClientesNuevo.jsx
+++ src/pages/ClientesNuevo.jsx
@@ -1,3 +1,34 @@
+import { useNavigate } from "react-router-dom";
+import Formulario from "../components/Formulario";
+
 export const ClientesNuevo = () => {
-  return <div>ClientesNuevo</div>;
+  // Hook para navegar de forma programatica
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-blue-900">Nuevo Cliente</h1>
+      <p className="mt-3">
+        Llene todos los datos para registrar un nuevo cliente
+      </p>
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white font-bold uppercase py-1 px-3"
+          onClick={() => navigate(-1)}
+        >
+          Volver
+        </button>
+      </div>
+      <div className="bg-white shadow rounded-sm md:w-3/4 p-6 mx-auto mt-10">
+        <form>
+          <Formulario />
+          <input
+            type="submit"
+            value="Registrar Cliente"
+            className="uppercase font-bold text-white bg-blue-800 w-full text-lg mt-5 p-3"
+          />
+        </form>
+      </div>
+    </>
+  );
 };
