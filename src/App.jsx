749db32995c19efadbd9f6baa728c641c294c176
Alejandro González Reyes <alexglzrys@gmail.com>
2023-03-20 11:04:21 -0600

    instalación de tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <h1>Administrador de Clientes</h1>
+      <h1 className="text-3xl font-bold text-center">
+        Administrador de Clientes
+      </h1>
     </div>
   );
 }
